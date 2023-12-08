4109d74361300302a1041d3daff76180d36a7155
DilawarSD <108129988+DilawarSD@users.noreply.github.com>
2023-12-08 14:06:42 +0000

    added/updated config file and h

--- .ccls-cache/@home@runner@Adaship-project/headers@config.h
+++ .ccls-cache/@home@runner@Adaship-project/headers@config.h
@@ -1,1 +1,32 @@
-//int config_main();
+// config.h
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <vector>
+#include <string>
+
+// Define a structure to represent boat configurations
+struct Boat {
+    std::string type;
+    int length;
+
+    // Constructor to initialize Boat
+    Boat(const std::string& boatType, int boatLength) : type(boatType), length(boatLength) {}
+};
+
+// Define a structure to represent game configuration
+class Config {
+public:
+    int boardSize;
+    std::vector<Boat> boats;
+
+    // Function declarations
+ static Config readConfig(const std::string& configFile);
+    std::vector<std::vector<char>> initializeBoard() const;
+
+// Function to initialize the board based on the configuration
+};
+
+
+#endif // CONFIG_H
--- adaship/config.cpp
+++ adaship/config.cpp
@@ -1,7 +1,42 @@
+#include <fstream>
+#include <sstream>
+#include <vector>
 #include <iostream>
+#include "../headers/config.h"
 
-int config_main() {
-  std::cout << "Hello World!\n";
-    return 0;
+
+Config Config::readConfig(const std::string& configFile) {
+    Config config;
+    std::ifstream file(configFile);
+
+    if (file.is_open()) {
+        std::string line;
+        while (std::getline(file, line)) {
+            size_t pos = line.find(":");
+            if (pos != std::string::npos) {
+                std::string key = line.substr(0, pos);
+                std::string value = line.substr(pos + 1);
+                if (key == "Board") {
+                    config.boardSize = std::stoi(value);
+                } else if (key == "Boat") {
+                    size_t boatPos = value.find(",");
+                    if (boatPos != std::string::npos) {
+                        std::string boatName = value.substr(0, boatPos);
+                        int boatSize = std::stoi(value.substr(boatPos + 1));
+                        config.boats.push_back(Boat(boatName, boatSize));
+                    }
+                }
+            }
+        }
+        file.close();
+    } else {
+        std::cerr << "Unable to open configuration file: " << configFile << std::endl;
+    }
+
+    return config;
+}
+
+std::vector<std::vector<char>> Config::initializeBoard() const {
+    return std::vector<std::vector<char>>(boardSize, std::vector<char>(boardSize, ' '));
 }
 

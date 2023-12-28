436ffc36dd027ab276a670ada77843e1a90dd1a4
DilawarSD <108129988+DilawarSD@users.noreply.github.com>
2023-12-28 12:55:46 +0000

    refactored and updated main file and added menu header

--- .ccls-cache/@home@runner@Adaship-project/main.cpp
+++ .ccls-cache/@home@runner@Adaship-project/main.cpp
@@ -2,40 +2,15 @@
 #include <vector>
 #include "headers/config.h"
 #include "headers/board.h"
-#include "headers/boats.h"
+#include "headers/ships.h"
+#include "headers/game.h"
+#include "headers/player.h"
+#include "headers/computer.h"
+#include "headers/menu.h"
+
 
 int main() {
-    // Specify the configuration file
-    std::string configFile = "adaship_config.ini";
+    Menu::executeMenuOption();
 
-    
-    Config config = Config::readConfig(configFile);
-
-    // Initialize manual placement board
-    std::vector<std::vector<char>> manualBoard = config.initializeBoard();
-
-    // Initialize automatic placement board
-    std::vector<std::vector<char>> autoBoard = config.initializeBoard();
-
-  std::vector<Boat> boats = config.getBoats();
-
-  // Place ships manually
-    for (const auto& boat : boats) {
-        Ships::manualPlaceShip(boat, config, manualBoard);
-    }
-
-    // Place ships automatically
-    for (const auto& boat : boats) {
-        Ships::autoPlaceShip(boat, config, autoBoard);
-    }
-
-    // Display manual placement board
-    std::cout << "Manual Board:\n";
-    displayBoard(manualBoard);
-
-    // Display automatic placement board
-    std::cout << "\nAutomatic Board:\n";
-    displayBoard(autoBoard);
-
-  return 0;
+    return 0;
 }
--- headers/menu.h
+++ headers/menu.h
@@ -1,1 +1,13 @@
-int menu_main();
+#ifndef MENU_H
+#define MENU_H
+
+#include <string>
+#include <vector>
+
+class Menu {
+public:
+    static void displayMenu();
+    static void executeMenuOption();
+};
+
+#endif // MENU_H

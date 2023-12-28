e61e59cdc0c1024b7a6387173e0619e3f2061c13
DilawarSD <108129988+DilawarSD@users.noreply.github.com>
2023-12-28 12:52:28 +0000

    updated menu file

--- .ccls-cache/@home@runner@Adaship-project/adaship@menu.cpp
+++ .ccls-cache/@home@runner@Adaship-project/adaship@menu.cpp
@@ -1,37 +1,56 @@
-#include <iostream> 
+#include "../headers/game.h"
+#include "../headers/ships.h"
+#include "../headers/board.h"
+#include "../headers/config.h"
+#include "../headers/player.h"
+#include "../headers/computer.h"
+#include "../headers/menu.h"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 void displaymenu() {
   std::cout << "=== Welcome to Adaship! ===\n";
   std::cout << "1. Player 1 VS Computer!\n";
-  std::cout <<"2.Rules\n";
-  std::cout <<"3.Exit\n";
-  std::cout <<"Enter your choice: " << std::endl;
+  std::cout <<"2.Exit\n";
+  std::cout << "============================\n";
 }
 
-int menu_main() {
-  int choice;
-    do {
-      displaymenu();
-     std::cin >> choice;
+void Menu::executeMenuOption() {
+    int choice;
+    Config config;
 
-     switch(choice) {
-       case 1:
-        std::cout << "Player 1 VS Computer!\n";
-       break;
-       case 2:
-        std::cout << "Rules\n";
-       break;
-       case 3:
-        std::cout << "Exiting...\n";
-       break;
-       default:
-        std::cout << "Invalid choice. Please try again.\n";
-     }
-      
-    } while (choice != 3);
-  
-    return 0;
-}
+     do {
+            displayMenu();
+            std::cout << "Enter your choice (1 or 2): ";
+            std::cin >> choice;
+            // Check if the input is an integer
+            if (std::cin.fail()) {
+                std::cin.clear();  // Clear the fail state
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Ignore remaining characters
+            }
+            switch (choice) {
+                case 1: {
+                    // Specify the configuration file
+                    std::string configFile = "adaship_config.ini";
+                    // Use the static function Config::readConfig
+                    config = Config::readConfig(configFile);
+                    // Create game instance
+                    Game game(config);
+                    // Run the existing gameplay logic
+                    game.play();
+
+                    break;
+                }
+                case 2:
+                    std::cout << "Exiting the program. Goodbye!\n";
+                    break;
+
+                default:
+                    std::cout << "Invalid choice. Please enter 1 or 2.\n";
+            }
+        } while (choice != 2);
+    }
 
 
 

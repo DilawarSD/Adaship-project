ff35a25fe9b6f2a60f66380f9aa70b9bf58c10b1
DilawarSD <108129988+DilawarSD@users.noreply.github.com>
2023-12-28 12:44:51 +0000

    updated game file to have game file code

--- headers/game.h
+++ headers/game.h
@@ -1,1 +1,47 @@
-int game_main();
+#ifndef GAME_H
+#define GAME_H
+
+#include "config.h"
+#include "ships.h"
+#include "player.h"
+#include "computer.h"
+#include <iostream> // Include the necessary header for Config class
+
+class Game {
+public:
+Game(const Config& config);
+
+std::pair<int, int> getUserMoveInput();
+
+std::vector<std::vector<char>> initializeBoard() const;
+
+void play();
+
+void playerMove(std::vector<std::vector<char>>& board);
+
+void computerMove(std::vector<std::vector<char>>& board);
+
+bool checkWin(const std::vector<std::vector<char>>& board) const;
+
+private:
+bool isValidMove(int row, int col, const std::vector<std::vector<char>>& board) const;
+
+void manualPlaceAllShips(const std::vector<Boat>& boats, const Config& config, std::vector<std::vector<char>>& board);
+
+void autoPlaceRemainingShips(const std::vector<Boat>& boats, const Config& config, std::vector<std::vector<char>>& board);
+
+void placeShips(std::vector<Boat>& boats, Ships& playerShips, Ships& computerShips, const Config& config);
+
+Ships& getShips();
+ 
+Ships playerShips;
+
+Ships computerShips;
+
+std::vector<std::vector<char>> playerBoard;
+
+std::vector<std::vector<char>> computerBoard;
+     
+    Config config;
+};
+#endif
